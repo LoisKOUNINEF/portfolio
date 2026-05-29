02736a7d0f6bc936e96d189ad09a8efcdab2109b
Lois KOUNINEF <lkouninef@gmail.com>
2026-05-29 03:29:52 +0200

    _

--- .github/workflows/scripts/deployment/docker-login.sh
+++ .github/workflows/scripts/deployment/docker-login.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
 docker_login() {
-  echo '{
-    "auths": {
-      "ghcr.io": {
-        "auth": "'"$DOCKER_AUTH"'"
-      }
-    }
-  }' > ~/.docker/config.json
+  echo '{"auths":{"ghcr.io":{"auth":"'"$DOCKER_AUTH"'"}}}' > ~/.docker/config.json
 
   docker login ghcr.io || exit 1
 }
